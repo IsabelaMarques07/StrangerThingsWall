b9b3a29c575e3a545890e23b8efd0d908b1b2328
IsabelaMarques07 <dev.isabelamarques@gmail.com>
2021-04-15 15:26:14 -0300

    Refactoring letters and footer

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const lights = document.getElementsByClassName('light')
 
+const colors = ["yellow", "blue", "pink", "green", "purple", "yellow", "red", "green", " ","green", "pink", "blue", "purple", "yellow", "red", "pink", "blue", "red", "purple", "red", "yellow", "green", "pink", "blue", "green", "purple", "yellow"]
+
+const letters = ["A", "B", "C", "D", "E","F", "G","H"," ", "I","J", "K","L", "M","N", "O","P", "Q","R", "S","T", "U","V", "W","X", "Y","Z"]
 
 document.querySelector('body').addEventListener('keydown', function(event) {
     let position = event.keyCode - 65
-    
     let colorLight = "glow-"
-    
-    const colors = ["yellow", "blue", "pink", "green", "purple", "yellow", "red", "green", "green", "pink", "blue", "purple", "yellow", "red", "pink", "blue", "red", "purple", "red", "yellow", "green", "pink", "blue", "green", "purple", "yellow"]
-
     on(position, colorLight+colors[position])
 
 });
@@ -25,8 +24,31 @@ function off(){
     }
 }
 
+var aux = 0;
+for(var i = 1; i <= 3; i++){
+
+    for(var j = aux; j < 9+ aux; j++){
+        if(letters[j] != " "){
+            var div = document.createElement("div")
+            div.setAttribute("class", "letter")
+        
+            var light = document.createElement("div")
+            light.setAttribute("class", "light "+colors[j])
+        
+            var letter = document.createElement("p")
+            letter.innerHTML= letters[j]
+        
+            document.getElementsByClassName("line-"+(i).toString())[0].appendChild(div)
+            div.appendChild(light)
+            div.appendChild(letter)
+        }
+    }
+    aux += 9
+}
 
 
-
-
-
+for(i = 0; i < 10; i++){
+    let line = document.createElement("div")
+    line.setAttribute("class", "line")
+    document.getElementsByClassName("wood-detail")[0].appendChild(line)
+}
